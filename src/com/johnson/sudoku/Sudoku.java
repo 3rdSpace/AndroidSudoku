2cc4987c5d5203ed459027e0a68ceeaeaae00341
amusal <amusal@tom.com>
2014-04-17 14:39:05 +0800

    Add new game difficulty selector

--- src/com/johnson/sudoku/Sudoku.java
+++ src/com/johnson/sudoku/Sudoku.java
@@ -1,15 +1,18 @@
 package com.johnson.sudoku;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.View.OnClickListener;
 
-public class Sudoku extends Activity implements OnClickListener {
+public class Sudoku extends Activity {
 
 	@Override
 	public void onCreate(Bundle savedInstanceState) {
@@ -17,28 +20,12 @@ public class Sudoku extends Activity implements OnClickListener {
 		setContentView(R.layout.main);
 		
 		// add button click listener
-		View continueBtn = super.findViewById(R.id.continue_btn);
-		continueBtn.setOnClickListener(this);
-		View newGameBtn = super.findViewById(R.id.new_game_btn);
-		newGameBtn.setOnClickListener(this);
-		View aboutBtn = super.findViewById(R.id.about_btn);
-		aboutBtn.setOnClickListener(this);
-		View exitBtn = super.findViewById(R.id.exit_btn);
-		exitBtn.setOnClickListener(this);
+		super.findViewById(R.id.continue_btn).setOnClickListener(btnClickListener);
+		super.findViewById(R.id.new_game_btn).setOnClickListener(btnClickListener);
+		super.findViewById(R.id.about_btn).setOnClickListener(btnClickListener);
+		super.findViewById(R.id.exit_btn).setOnClickListener(btnClickListener);
 	}
 
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.about_btn:
-			Intent i = new Intent(this, About.class);
-			super.startActivity(i);
-			break;
-		case R.id.exit_btn:
-			System.exit(0);
-		}
-	}
-	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
 		super.onCreateOptionsMenu(menu);
@@ -56,4 +43,52 @@ public class Sudoku extends Activity implements OnClickListener {
 		}
 		return false;
 	}
+	
+	/**
+	 * OnclickListener
+	 */
+	private final OnClickListener btnClickListener = new OnClickListener() {
+
+		@Override
+		public void onClick(View v) {
+			switch (v.getId()) {
+			case R.id.new_game_btn:
+				openNewGameDialog();
+				break;
+			case R.id.about_btn:
+				Intent i = new Intent(Sudoku.this, About.class);
+				Sudoku.this.startActivity(i);
+				break;
+			case R.id.exit_btn:
+				System.exit(0);
+			}
+		}
+		
+		/**
+		 * Open a difficulty selecting dialog for new game
+		 */
+		private void openNewGameDialog() {
+			new AlertDialog.Builder(Sudoku.this)
+					.setTitle(R.string.new_game_title)
+					.setItems(R.array.difficulty, new DialogInterface.OnClickListener() {
+						@Override
+						public void onClick(DialogInterface dialog, int which) {
+							startNewGame(which);
+						}
+					})
+					.show();
+		}
+		
+		/**
+		 * Start new game
+		 * @param option
+		 * 		selected difficulty option
+		 */
+		private void startNewGame(int option) {
+			Log.d(Sudoku.class.getName(), "clicked on " + option);
+			// TODO: start game
+		}
+	};
+	
+	
 }	
